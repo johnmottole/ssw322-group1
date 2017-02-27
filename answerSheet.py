e54df3c930f99bc9b02c90e31fc5970bb0f59766
Jacob Ulasevich <ulasevich.jacob@gmail.com>
2017-02-27 10:40:46 -0500

    answerSheet class

---
+++ answerSheet.py
@@ -0,0 +1,73 @@
+'''
+Header here
+'''
+
+#Imports
+from random import randint
+
+#SUPER CLASS: answerSheet
+class answerSheet(object):
+    '''
+    answer sheet is a parent class.  It holds:
+    answers
+    name
+    tag
+    '''
+    
+    #Initialize
+    def __init__(self):
+        self.__answers = []
+        self.__name = ""
+        self.__tag = ""
+        
+    @property
+    def answers(self):
+        return self.__answers
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def tag(self):
+        return self.__tag
+    
+    @answers.setter
+    def setAnswer(self, newAnswers):
+        self.__answers = newAnswers
+        
+    @name.setter
+    def setName(self, newName):
+        self.__name = newName
+    
+    @tag.setter
+    def setTag(self, newTag):
+        self.__tag = newTag
+        
+
+class grader(answerSheet):
+    '''
+    grader is a child class of answerSheet.  It holds:
+    function to grade
+    grade
+    '''
+    
+    def __init__(self):
+        super().__init__()
+        self.__grade = 0
+        
+    @property
+    def grade(self):
+        return grader
+    
+    #Insert grader function here
+    
+class tabulator(answerSheet):
+    '''tabulator is a child class of answerSheet.  It holds:
+    
+    '''
+    
+    def __init__(self):
+        super().__init__()
+        
+    
